f99ee99c0bba7e5217ced74550ff802073752084
parsanoroozi <parsan6347@gmail.com>
2022-09-03 14:05:42 +0430

    first message

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,21 @@
+import asiox from "axios";
+
+const axios = require("axios");
+
+const Base_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${Base_URL}/${url}`, options);
+
+  return data;
+};
